860e875fbc96b483196cba4a70d925675f987677
Sakthivel Subbaian <sak_michi@outlook.com>
2019-06-21 12:16:11 +0530

    loading sprites by random

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,168 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    const int nTT = 3; // HARD CODED VALUE
+    const int nBT = 5;//  HARD CODED VALUE
+    // Tiles
+    public GameObject[] TT;                     //Top Tiles
+    public GameObject[] BT;                     //Bottom Tiles
+    // Tile positions
+    Vector2[] TTPositions = new Vector2[nTT];
+    Vector2[] BTPositions = new Vector2[nBT];
+    // Sprite names and thier counts
+    List<string> animalNames = new List<string>();
+    List<int> animalCount = new List<int>();
+    // Current animal and the selected animal
+    protected int currentAnimalType;
+    protected int selectedAnimal;
+    //
+    protected string[] TTAnimalName = new string[nTT];
+    protected string[] BTAnimalName = new string[nBT];
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (TT.Length != nTT || BT.Length != nBT)
+            Debug.LogError("Mismatching tiles!");
+
+        getInitialTilePositions();
+        ProcessSpritesFolder();
+        LoadChoosenAnimalsToTiles();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
+    //Store all the tiles initial poistions 
+    void getInitialTilePositions()
+    {
+        for (int i = 0; i < nTT; i++)
+            TTPositions[i] = TT[i].transform.position;
+
+        for (int i = 0; i < nBT; i++)
+            BTPositions[i] = BT[i].transform.position;
+    }
+    //Fetch all the animal types and thier counts from the sprites folder
+    private void ProcessSpritesFolder()
+    {
+        var sprites = Resources.LoadAll("Sprites", typeof(Sprite));
+        List<string> imagesNameList = new List<string>();
+        List<string> animalTypeList = new List<string>();
+        // fetch sprite name
+        foreach (var t in sprites)
+        {
+            imagesNameList.Add(t.name);
+        }
+        bool insertType = false;
+        //Extract the animal name from sprite name
+        foreach (var str in imagesNameList)
+        {
+            insertType = true;
+            //Split the string into multiple strings by '_'
+            string[] image_name = str.Split('_');
+            foreach (var type in animalTypeList)
+            {
+                if (image_name[0].Equals(type))
+                {
+                    insertType = false;
+                    break;
+                }
+            }
+            if (insertType)
+            {
+                animalTypeList.Add(image_name[0]);
+            }
+        }
+        //Count number of sprites for each animal type
+        for (int i = 0; i < animalTypeList.Count; i++)
+        {
+            int count = 0;
+            foreach (var str in imagesNameList)
+            {
+                //Split the string into multiple strings by '_'
+                string[] image_name = str.Split('_');
+                if (animalTypeList[i].Equals(image_name[0]))
+                    count++;
+            }
+            animalCount.Add(count);
+        }
+        //Add '_' to each animal name, it helps makeup the name later
+        for (int i = 0; i < animalTypeList.Count; i++)
+        {
+            animalNames.Add(string.Concat(animalTypeList[i], "_"));
+        }
+        //print the animals and their count
+        for (int i = 0; i < animalTypeList.Count; i++)
+            Debug.Log(animalNames[i] + " " + animalCount[i]);
+    }
+    //Choose animals for top row tiles
+    private void ChooseAnimalsForTopRowTiles()
+    {
+        // number of animals
+        int nAnimal = animalNames.Count;
+        // Choose the current animal
+        do
+        {
+            currentAnimalType = Random.Range(0, nAnimal);
+        } while (animalCount[currentAnimalType] < 3);
+        // number of animals of that kind
+        int nCount = animalCount[currentAnimalType];
+        // Fill the first tile with the current animal
+        TTAnimalName[0] = string.Concat("Sprites/", animalNames[currentAnimalType], Random.Range(1, nCount));
+        // Fill the second tile with the current animal and avoid repetition
+        do
+        {
+            TTAnimalName[1] = string.Concat("Sprites/", animalNames[currentAnimalType], Random.Range(1, nCount));
+        } while (TTAnimalName[0].Equals(TTAnimalName[1]) == true);
+
+        TTAnimalName[2] = "Question";
+        //print the animals and their count
+            Debug.Log(TTAnimalName[0] + ", " + TTAnimalName[1] + ", " + TTAnimalName[2]);
+    }
+    //Choose animals for bottom row tiles
+    private void ChooseAnimalsForBottomRowTiles()
+    {
+        // number of animals
+        int nAnimal = animalNames.Count;
+        //Bottom tile animal types
+        int BT_AnimalType = currentAnimalType;
+        // Fill the bottom tiles with the radom animals
+        for (int i = 0; i < nBT; i++)
+        {
+            do
+            {
+                BT_AnimalType = Random.Range(0, nAnimal);
+            } while (BT_AnimalType ==currentAnimalType);
+            // number of animals of that kind
+            int count1 = animalCount[BT_AnimalType];
+            BTAnimalName[i] = string.Concat("Sprites/", animalNames[BT_AnimalType], Random.Range(1, count1));
+        }
+
+        int index = Random.Range(0, nBT);
+        int count2 = animalCount[currentAnimalType];
+        // Fill one of the bottom tiles with the top animals
+        do
+        {
+            BTAnimalName[index] = string.Concat("Sprites/", animalNames[currentAnimalType], Random.Range(1, count2));
+        } while (BTAnimalName[index].Equals(TTAnimalName[0]) == true ||
+                 BTAnimalName[index].Equals(TTAnimalName[1]) == true);
+
+        for (int i = 0; i < nBT; i++)
+            Debug.Log(BTAnimalName[i]);
+        //Debug.Log("animal arr:""animal arr:"+ animal_til);
+    }
+    //Choose animals for bottom row tiles
+    private void LoadChoosenAnimalsToTiles()
+    {
+        ChooseAnimalsForTopRowTiles();
+        ChooseAnimalsForBottomRowTiles();
+        for (int i = 0; i < nTT; i++)
+            TT[i].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(TTAnimalName[i]);
+        for (int i = 0; i < nBT; i++)
+            BT[i].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(BTAnimalName[i]);
+    }
+}
---
+++ GridManager.cs
@@ -0,0 +1,339 @@
+﻿//using System;
+//using System;
+//using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+
+    //public Sprite[] spriteList;
+
+    const int nAnimalTiles = 8;
+
+    GameObject[] tile_Frames = new GameObject[3];
+    Vector2[] tilePositions = new Vector2[nAnimalTiles];
+    Vector3[] tileScale = new Vector3[nAnimalTiles];
+    GameObject[] tiles = new GameObject[nAnimalTiles];
+
+    List<string> animalNames = new List<string>();
+    //private string[] animalNames = {"cat_", "cow_", "dog_", "elephant_", "goat_", "horse_",
+    //                                "lion_", "monkey_", "rhino_", "tiger_"};
+    protected string currentAnimalType;
+    protected string selectedAnimal;
+    protected string[] animal_til = new string[nAnimalTiles];
+
+    Collider2D m_Collider;
+    Vector3 m_Center;
+    Vector3 m_Size, m_Min, m_Max;
+    Vector3 selected_position;
+    Vector3 target_position;
+    Vector3 mid_position;
+    Vector3 final_position;
+
+    GameObject selected = null;
+    static float waitSeconds = 1f;
+    float secondsToWait = 1f;
+    bool rightSelection = false;
+    bool reloadTiles = false;
+    bool reachMidPoint = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ProcessSpritesFolder();
+        PrepareSprites();
+        Debug.Log("Grid position: " + transform.position);
+        GenerateGrid();
+    }
+    private void ProcessSpritesFolder()
+    {
+        var sprites = Resources.LoadAll("Sprites", typeof(Sprite));
+        List<string> imagesNameList = new List<string>();
+        List<string> animalTypeList = new List<string>();
+        foreach (var t in sprites)
+        {
+            imagesNameList.Add(t.name);
+        }
+        bool insertType = false;
+        foreach (var str in imagesNameList)
+        {
+            insertType = true;
+            string[] image_name= str.Split('_');
+            foreach (var type in animalTypeList)
+            {
+                if (image_name[0].Equals(type))
+                {
+                    insertType = false;
+                    break;
+                }
+            }
+            if (insertType)
+                animalTypeList.Add(image_name[0]);
+        }
+        for (int i=0; i< animalTypeList.Count; i++)
+        {
+            animalNames.Add(string.Concat(animalTypeList[i], "_"));
+        }
+        foreach (var type in animalNames)
+            Debug.Log(type);
+    }
+    private void PrepareSprites()
+    {
+
+    }
+    private void GenerateGrid()
+    {
+        setupFrames();
+        fixedTilesPosition();
+        fixedTilesScale();
+        setupTilesToSprites();
+        setTilesTheirPositionAndScale();
+        LoadSpritesToTiles();
+        final_position = tiles[2].transform.position;
+    }
+    private void fixedTilesPosition()
+    {
+        // Row 1
+        tilePositions[0] = new Vector2(-3f, 1.5f);
+        tilePositions[1] = new Vector2(0, 1.5f);
+        tilePositions[2] = new Vector2(3f, 1.5f);
+
+        // Row 2
+        tilePositions[3] = new Vector2(-3.6f, -1.5f);
+        tilePositions[4] = new Vector2(-1.8f, -1.5f);
+        tilePositions[5] = new Vector2(0, -1.5f);
+        tilePositions[6] = new Vector2(1.8f, -1.5f);
+        tilePositions[7] = new Vector2(3.6f, -1.5f);
+    }
+    private void fixedTilesScale()
+    {
+        // Row 1
+        tileScale[0] = new Vector3(2.2F, 2.2F, 0);
+        tileScale[1] = new Vector3(2.2F, 2.2F, 0);
+        tileScale[2] = new Vector3(2.2F, 2.2F, 0);
+
+        // Row 2
+        tileScale[3] = new Vector3(1.1F, 1.1F, 0f);
+        tileScale[4] = new Vector3(1.1F, 1.1F, 0f);
+        tileScale[5] = new Vector3(1.1F, 1.1F, 0f);
+        tileScale[6] = new Vector3(1.1F, 1.1F, 0f);
+        tileScale[7] = new Vector3(1.1F, 1.1F, 0f);
+    }
+    private void setupTilesToSprites()
+    {
+        GameObject referenceTile = (GameObject)Instantiate(Resources.Load("ImageTile"));
+        for (int i = 0; i < nAnimalTiles; i++)
+        {
+            tiles[i] = (GameObject)Instantiate(referenceTile, transform);
+            tiles[i].GetComponent<SpriteRenderer>().sortingOrder = 20;
+        }
+        Destroy(referenceTile);
+    }
+    private void setTilesTheirPositionAndScale()
+    {
+        for(int i = 0; i<nAnimalTiles; i++)
+        {
+            tiles[i].transform.position = tilePositions[i];
+            tiles[i].transform.localScale = tileScale[i];
+            Debug.Log("localScale : " + tiles[i].transform.localScale);
+        }
+    }
+    private void setupFrames()
+    {
+        GameObject referenceTileFrame = (GameObject)Instantiate(Resources.Load("frame3"));
+        referenceTileFrame.transform.localScale += new Vector3(1.5F, 1.5F, 0);
+        // Row 1
+        tile_Frames[0] = (GameObject)Instantiate(referenceTileFrame, transform);
+        tile_Frames[0].transform.position = new Vector2(-3f, 1.5f);
+
+        tile_Frames[1] = (GameObject)Instantiate(referenceTileFrame, transform);
+        tile_Frames[1].transform.position = new Vector2(0, 1.5f);
+
+        tile_Frames[2] = (GameObject)Instantiate(referenceTileFrame, transform);
+        tile_Frames[2].transform.position = new Vector2(3f, 1.5f);
+
+        Destroy(referenceTileFrame);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        UserInputHandler();
+        TileMovement();
+        PlannedReloadOfTiles();
+    }
+    private void LoadSpritesToTiles()
+    {
+        ChooseAnimals();
+        for (int i = 0; i < nAnimalTiles; i++)
+        {
+            tiles[i].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(animal_til[i]);
+        }
+    }
+    private void ChooseAnimals()
+    {
+        currentAnimalType = animalNames[Random.Range(0, 10)];
+        animal_til[0] = string.Concat("Sprites/", currentAnimalType, Random.Range(1, 6));
+        do
+        {
+            animal_til[1] = string.Concat("Sprites/", currentAnimalType, Random.Range(1, 6));
+        } while (animal_til[0].Equals(animal_til[1]) == true);
+
+        animal_til[2] = "Question";
+
+        string row2_AnimalType;
+        for (int i = 3; i < nAnimalTiles; i++)
+        {
+            do
+            {
+                row2_AnimalType = animalNames[Random.Range(0, 10)];
+            } while (row2_AnimalType.Equals(currentAnimalType));
+            animal_til[i] = string.Concat("Sprites/", row2_AnimalType, Random.Range(1, 6));
+        }
+
+        int index = Random.Range(3, nAnimalTiles);
+        do
+        {
+            animal_til[index] = string.Concat("Sprites/", currentAnimalType, Random.Range(1, 6));
+        } while (animal_til[index].Equals(animal_til[0]) == true ||
+                 animal_til[index].Equals(animal_til[1]) == true);
+
+        //for (int i = 0; i < 6; i++)
+        //    Debug.Log(animal_til[i]);
+        //Debug.Log("animal arr:""animal arr:"+ animal_til);
+    }
+    private void loadUpperRow()
+    {
+        //currentAnimalType = animalNames[Random.Range(0, 10)];
+        //Debug.Log("curr animal :"+ currentAnimalType);
+    }
+    GameObject ClickSelect()
+    {
+        if (selected != null) return selected;
+
+        //Converting Mouse Pos to 2D (vector2) World Pos
+        //Debug.Log("mousePos :" + Input.mousePosition + "world Pos :" + Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), -Vector2.up);
+
+        if (hit.collider != null && (hit.transform.gameObject.GetComponent<SpriteRenderer>()) != null)
+        {
+//            Debug.Log(hit.transform.gameObject.GetComponent<SpriteRenderer>().sprite.name);
+            //Fetch the Collider from the GameObject
+            m_Collider = hit.collider;
+            //           colliderInfo();
+            //hit.transform.gameObject.GetComponent<SpriteRenderer>().sprite
+            return hit.transform.gameObject;
+        }
+        else return null;
+    }
+    private void colliderInfo()
+    {
+        //Fetch the center of the Collider volume
+        m_Center = m_Collider.bounds.center;
+        //Fetch the size of the Collider volume
+        m_Size = m_Collider.bounds.size;
+        //Fetch the minimum and maximum bounds of the Collider volume
+        m_Min = m_Collider.bounds.min;
+        m_Max = m_Collider.bounds.max;
+
+        //Output to the console the center and size of the Collider volume
+        Debug.Log("Collider Center : " + m_Center);
+        Debug.Log("Collider Size : " + m_Size);
+        Debug.Log("Collider bound Minimum : " + m_Min);
+        Debug.Log("Collider bound Maximum : " + m_Max);
+    }
+    void PlannedReloadOfTiles()
+    {
+        if (reloadTiles )
+        {
+            secondsToWait -= Time.deltaTime;  // T.dt is secs since last update
+            if (secondsToWait <= 0)
+            {
+                reloadTiles = false;
+                secondsToWait = waitSeconds;
+                // do thing here:
+                setTilesTheirPositionAndScale();
+                LoadSpritesToTiles();
+            }
+        }
+        return;
+    }
+    bool CheckSelectedTile(GameObject Selected = null)
+    {
+        bool output = false;
+        if (Selected == null) return output;
+
+        string tile_name = Selected.GetComponent<SpriteRenderer>().sprite.name;
+        string[] tile_name_list = tile_name.Split('_');
+        string[] currentAnimal_name_list = currentAnimalType.Split('_');
+//            Debug.Log("tile_name_list : " + tile_name_list[0] + "   currentAnimalType : " + currentAnimal_name_list[0]);
+        if (tile_name_list[0].Equals(currentAnimal_name_list[0]))
+            output = true;
+        return output;
+    }
+    void UserInputHandler()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
+        {
+            selected = ClickSelect();
+            TileSelection();
+        }
+    }
+    void TileSelection()
+    {
+        if (selected)
+        {
+            rightSelection = CheckSelectedTile(selected);
+            selected_position = selected.transform.position;
+
+            if (rightSelection)
+                target_position = final_position;
+            else
+            {
+                mid_position = (selected_position + final_position) / 2f;
+                Debug.Log("selected_position: " + selected_position +
+                    " final_position : " + final_position +
+                    " mid_position : " + mid_position);
+                target_position = mid_position;
+                reachMidPoint = false;
+            }
+        }
+    }
+    void TileMovement()
+    {
+        if (selected)
+        {
+            selected.GetComponent<SpriteRenderer>().sortingOrder = 98;
+
+            selected.transform.position = Vector3.LerpUnclamped(selected.transform.position, target_position, Time.deltaTime);
+   //         selected.transform.localScale = Vector3.Slerp(selected.transform.localScale, tiles[2].transform.localScale, Time.deltaTime);
+
+            if (Vector3.Distance(selected.transform.position, target_position) < 0.2f)
+            {
+                if (rightSelection)
+                {
+                    SpriteRenderer sr = tiles[2].GetComponent<SpriteRenderer>();
+                    if (!(sr.sprite.name).Equals("Question")) return;
+                    sr.sprite = selected.GetComponent<SpriteRenderer>().sprite;
+                    selected.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Transparent");
+                    selected.GetComponent<SpriteRenderer>().sortingOrder = 20;
+                    selected = null;
+                    reloadTiles = true;
+                }
+                else if (reachMidPoint == true)
+                {
+                    selected.transform.position = selected_position;
+                    selected.GetComponent<SpriteRenderer>().sortingOrder = 20;
+                    selected = null;
+                }
+                else
+                {
+                    reachMidPoint = true;
+                    target_position = selected_position;
+                }
+            }
+            //Debug.Log("Time.deltaTime : " + (Time.deltaTime + 0.05f));
+        }
+    }
+}
